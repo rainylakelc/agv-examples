9f4caf57c1418d26e472487ec70911956ef217d6
Joshua Marshall <joshua.marshall@queensu.ca>
2024-11-14 15:14:14 -0500

    Updated RLS example to show raw measurement error

--- recursive_least_squares.py
+++ recursive_least_squares.py
@@ -53,7 +53,7 @@ x_hat[:, 0] = np.array([0.0, 0.0])
 # Initial covariance matrix
 P_hat[:, :, 0] = 10.0 * np.eye(2)
 # Measurement noise covariance matrix
-SIGMA = 0.5
+SIGMA = 1.0
 R = SIGMA**2 * np.eye(2)
 # Measurement matrix (i.e., GPS coordinates)
 H = np.eye(2)
@@ -80,8 +80,8 @@ plt.rc("savefig", bbox="tight")
 
 # Plot the true and estimated robot location
 plt.figure()
-plt.plot(x[0], x[1], "C0o", label="True")
-plt.plot(x_hat[0, :], x_hat[1, :], "C1-", label="Estimated")
+plt.plot(x[0], x[1], "C4o", label="True")
+plt.plot(x_hat[0, :], x_hat[1, :], "C0-", label="Estimated")
 plt.xlabel(r"$x_1$ [m]")
 plt.ylabel(r"$x_2$ [m]")
 plt.legend()
@@ -106,7 +106,8 @@ plt.fill_between(
     alpha=0.2,
     label=str(100 * (1 - ALPHA)) + r" \% Confidence",
 )
-plt.plot(t, x[0] - x_hat[0, :], "C0", label="Error")
+plt.plot(t, x[0] - x_hat[0, :], "C0", label="Estimator Error")
+plt.plot(t, x[0] - y[0, :], "C1", label="Raw Measurement Error")
 plt.ylabel(r"$e_1$ [m]")
 plt.setp(ax1, xticklabels=[])
 plt.grid(color="0.95")
@@ -121,11 +122,11 @@ plt.fill_between(
     alpha=0.2,
     label=str(100 * (1 - ALPHA)) + r" \% Confidence",
 )
-plt.plot(t, x[1] - x_hat[1, :], "C0", label="Error")
+plt.plot(t, x[1] - x_hat[1, :], "C0", label="Estimator Error")
+plt.plot(t, x[1] - y[1, :], "C1", label="Raw Measurement Error")
 plt.ylabel(r"$e_2$ [m]")
 plt.grid(color="0.95")
 plt.xlabel(r"$t$ [s]")
 plt.show()
-
 
 # %%
